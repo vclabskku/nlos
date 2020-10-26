7a119bc36f2a7d9779e923277e17da3a6a0ad217
kth0522 <rlaxogh369@gmail.com>
2020-10-27 00:03:39 +0900

    galvanometer code

--- data_generation/real_data/galvanometer/galvanometer.py
+++ data_generation/real_data/galvanometer/galvanometer.py
@@ -1,6 +1,19 @@
 
 import numpy as np
 import nidaqmx
+import math
+
+def calculate_x_voltage(x):
+    a = -0.39669946
+    b = 19.5017455
+    return a*math.degrees(math.atan((279-x)/166))+b
+
+def calculate_y_voltage(y):
+    a = -0.39294275
+    b = -0.043263
+    return a*math.degrees(math.atan((44.5-y)/252))+b
+
+
 
 class Galvanometer():
 
@@ -14,19 +27,27 @@ class Galvanometer():
         # add y-axis channel
         self.task.ao_channels.add_ao_voltage_chan("Dev1/ao1")
 
+        self.width = 187
+        self.height = 90.5
+
         self.num_grid = self.config["num_grid"] # num_grid x num_grid scanning
         self.voltage_range = self.config["voltage_range"] # [-max_voltage, max_voltage]
 
+        self.x_term = self.width / self.num_grid
+        self.y_term = self.height / self.num_grid
+
         self.voltages = np.linspace(self.voltage_range[0], self.voltage_range[1], self.num_grid)
         self.count = 0
+
+
 
     def step(self):
 
         x_index = self.count // self.num_grid
         y_index = self.count % self.num_grid
 
-        x_voltage = self.voltages[x_index]
-        y_voltage = self.voltages[y_index]
+        x_voltage = calculate_x_voltage(x_index * self.x_term)
+        y_voltage = calculate_y_voltage(y_index * self.y_term)
 
         self.task.write([x_voltage, y_voltage], auto_start=True)
         self.count += 1
